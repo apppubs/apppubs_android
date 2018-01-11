83a50fb5bf4f4404fd21bd70177cb9ecd2b618b2
张稳 <1017068930@qq.com>
2018-01-11 17:52:35 +0800

    处理异常

--- mportal/src/main/java/com/apppubs/d20/net/WMHHttpClientDefaultImpl.java
+++ mportal/src/main/java/com/apppubs/d20/net/WMHHttpClientDefaultImpl.java
@@ -3,6 +3,7 @@ package com.apppubs.d20.net;
 import android.util.Log;
 
 import com.apppubs.d20.util.JSONResult;
+import com.apppubs.d20.util.StringUtils;
 import com.google.gson.JsonParseException;
 
 import org.json.JSONException;
@@ -21,38 +22,40 @@ import okhttp3.Response;
 
 public class WMHHttpClientDefaultImpl implements WMHHttpClient {
 
-	private OkHttpClient mOkHttpClient;
-	public WMHHttpClientDefaultImpl(){
-		mOkHttpClient = new OkHttpClient();
-	}
-	@Override
-	public void GET(String pattern, Object[] params, final WMHRequestListener listener) {
-		String url  = String.format(pattern,params);
-		final Request request = new Request.Builder()
-				.url(url)
-				.build();
-		Call call = mOkHttpClient.newCall(request);
-		call.enqueue(new Callback() {
-			@Override
-			public void onFailure(Call call, IOException e) {
-				e.printStackTrace();
-				listener.onDone(null,WMHHttpErrorCode.IO_EXCEPTION);
+    private OkHttpClient mOkHttpClient;
 
-			}
+    public WMHHttpClientDefaultImpl() {
+        mOkHttpClient = new OkHttpClient();
+    }
 
-			@Override
-			public void onResponse(Call call, Response response) throws IOException {
-				String responseStr = response.body().string();
-				JSONResult jr = null;
-				try{
-					 jr = JSONResult.compile(responseStr);
-					listener.onDone(jr, null);
-				}catch (JsonParseException e){
-					Log.v("WMHHttpClient",responseStr);
-					e.printStackTrace();
-					listener.onDone(null,WMHHttpErrorCode.JSON_PARSE_ERROR);
-				}
-			}
-		});
-	}
+    @Override
+    public void GET(String pattern, Object[] params, final WMHRequestListener listener) {
+        String url = String.format(pattern, params);
+        final Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Call call = mOkHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                listener.onDone(null, WMHHttpErrorCode.IO_EXCEPTION);
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseStr = response.body().string();
+                JSONResult jr = null;
+                try {
+                    jr = JSONResult.compile(responseStr);
+                    listener.onDone(jr, null);
+                } catch (JsonParseException e) {
+                    Log.v("WMHHttpClient", responseStr);
+                    e.printStackTrace();
+                    listener.onDone(null, WMHHttpErrorCode.JSON_PARSE_ERROR);
+                }
+            }
+        });
+    }
 }
